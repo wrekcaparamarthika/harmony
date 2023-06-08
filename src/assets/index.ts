8f3de47299049521e8e7808f7371230ea4c19082
Epen Flow <53342177+epenflow@users.noreply.github.com>
2023-06-08 22:17:20 +0800

    update ticket

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,6 @@ import Merch1 from './Merch1.png';
 import Merch2 from './Merch2.png';
 import Merch3 from './Merch3.png';
 import Merch4 from './Merch4.png';
-
-export { Merch1, Merch2, Merch3, Merch4 };
+import presale from './presale.png';
+import spot from './spot.png';
+export { Merch1, Merch2, Merch3, Merch4, presale, spot };
--- src/views/components/Ticket.tsx
+++ src/views/components/Ticket.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
-
+import styled from '@emotion/styled';
 import { Global } from '@emotion/react';
 import globalInject from '../style/DefaultStyle';
 import Artwork from './Artwork';
 import NavHeader from './NavHeader';
-
-const Ticket = () => {
+import { presale, spot } from '../../assets';
+import { NavLink } from 'react-router-dom';
+const TicketStyle = styled.div`
+	position: fixed;
+	bottom: 20%;
+	left: 50%;
+	transform: translateX(-50%);
+	a {
+		section {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			img {
+				width: 280px;
+			}
+		}
+	}
+`;
+const Ticket = (): React.JSX.Element => {
 	return (
 		<React.Fragment>
 			<Global styles={globalInject} />
 			<NavHeader title='ticket' />
+			<TicketStyle>
+				<NavLink
+					to={
+						'https://api.whatsapp.com/send/?phone=%2B6281239277187&text&type=phone_number&app_absent=0'
+					}>
+					<section>
+						<img src={presale} />
+					</section>
+				</NavLink>
+				<NavLink
+					to={
+						'https://api.whatsapp.com/send/?phone=%2B6287758706555&text&type=phone_number&app_absent=0'
+					}>
+					<section>
+						<img src={spot} />
+					</section>
+				</NavLink>
+			</TicketStyle>
 			<Artwork />
 		</React.Fragment>
 	);
