9877abc16d37b4b55cf402f8a5402a1d73383b13
Epen Flow <53342177+epenflow@users.noreply.github.com>
2023-06-11 17:42:00 +0800

    update Default#3

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,5 +8,24 @@ const contactPerson = [
 		cp: 'https://api.whatsapp.com/send/?phone=%2B6287758706555&text&type=phone_number&app_absent=0',
 	},
 ];
-
-export { contactPerson };
+const latarText = {
+	paragraph1: `Mengacu pada dampak dari Pandemi Covid - 19 di indonesia
+	yang hampir 3 (tiga) tahun melanda dunia baik Ekonomi,
+	Sosial maupun Budaya menjadi tidak stabil, dimana banyak
+	masyarakat meminimalisir untuk berinterkasi antara satu
+	sama lain, hal ini membuat semua sektor baik Ekonomi,
+	Sosial dan Budaya menjadai down.`,
+	paragraph2: `Melihat perkembangan kasus Covid - 19 di Indonesia,
+	khususnya di bali semakin menurun dan kelonggaran
+	peraturan PPKM membuat interaksi masyarakat satu sama
+	lain mulai bergeliat. Namun hal tersebut belum dapat
+	membuat sektor Ekonomi, Sosial, dan Budaya berjalan
+	optimal.`,
+	paragraph3: `Maka, kami selaku generasi penerus ingin membuat suatu
+	program yang mampu mendorong sektor Ekonomi, Sosial dan
+	Budaya agar dapat bergeliat kembali secara normal dengan
+	memberdayakan UMKM, menumbuhkan kesadaran sosial bagi
+	masyarakat, serta menggeliatkan kembali sektor
+	kebudayaan di masyarakat yang kami rangkum dalam tema`,
+};
+export { contactPerson, latarText };
--- src/views/layouts/Default.tsx
+++ src/views/layouts/Default.tsx
@@ -3,6 +3,7 @@ import { Global } from '@emotion/react';
 import globalInject from '../style/DefaultStyle';
 import styled from '@emotion/styled';
 import Artwork from '../components/Artwork';
+import { latarText } from '../../constants';
 const MainStyle = styled.main`
 	display: flex;
 	flex-direction: column;
@@ -10,26 +11,7 @@ const MainStyle = styled.main`
 	justify-content: center;
 	padding: 1rem;
 `;
-const latarText = {
-	paragraph1: `Mengacu pada dampak dari Pandemi Covid - 19 di indonesia
-	yang hampir 3 (tiga) tahun melanda dunia baik Ekonomi,
-	Sosial maupun Budaya menjadi tidak stabil, dimana banyak
-	masyarakat meminimalisir untuk berinterkasi antara satu
-	sama lain, hal ini membuat semua sektor baik Ekonomi,
-	Sosial dan Budaya menjadai down.`,
-	paragraph2: `Melihat perkembangan kasus Covid - 19 di Indonesia,
-	khususnya di bali semakin menurun dan kelonggaran
-	peraturan PPKM membuat interaksi masyarakat satu sama
-	lain mulai bergeliat. Namun hal tersebut belum dapat
-	membuat sektor Ekonomi, Sosial, dan Budaya berjalan
-	optimal.`,
-	paragraph3: `Maka, kami selaku generasi penerus ingin membuat suatu
-	program yang mampu mendorong sektor Ekonomi, Sosial dan
-	Budaya agar dapat bergeliat kembali secara normal dengan
-	memberdayakan UMKM, menumbuhkan kesadaran sosial bagi
-	masyarakat, serta menggeliatkan kembali sektor
-	kebudayaan di masyarakat yang kami rangkum dalam tema`,
-};
+
 const LatarStyle = styled.div`
 	position: fixed;
 	top: 10%;
@@ -39,7 +21,6 @@ const LatarStyle = styled.div`
 	margin: 1rem;
 	div {
 		background-color: #a6373e;
-		border-radius: 2.5rem;
 		padding: 1rem;
 		height: 10rem;
 		overflow-y: auto;
