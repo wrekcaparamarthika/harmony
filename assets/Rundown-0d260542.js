9274dc031c16302b9ccc273dea51846edd98e524
Epen Flow <53342177+epenflow@users.noreply.github.com>
2023-06-14 17:22:12 +0800

    update artis#2

--- assets/Rundown-0d260542.js
+++
@@ -1,5 +0,0 @@
-import{n as s,j as n,R as e,G as o,g as r,N as a,A as i}from"./index-4038497e.js";const l=s.div`
-	padding: 0.25rem;
-	display: flex;
-	justify-content: center;
-`,d=({title:t="title"})=>n.jsx(l,{children:n.jsx("h1",{children:t})}),j=()=>n.jsxs(e.Fragment,{children:[n.jsx(o,{styles:r}),n.jsx(a,{title:"Rundown"}),n.jsx(d,{title:"rundown coming soon!"}),n.jsx(i,{})]});export{j as Rundown};
