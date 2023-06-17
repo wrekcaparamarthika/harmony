f7413e4906070d9936e8374b290d4743e0880802
Epen Flow <53342177+epenflow@users.noreply.github.com>
2023-06-18 00:42:48 +0800

    update lineup#2

--- assets/index-85b66e65.js
+++
@@ -1,1 +0,0 @@
-const p=[{name:"suralaga",cp:"https://api.whatsapp.com/send/?phone=%2B6281239277187&text&type=phone_number&app_absent=0"},{name:"devani rata",cp:"https://api.whatsapp.com/send/?phone=%2B6287758706555&text&type=phone_number&app_absent=0"}];export{p as c};
